5cba4e32834fa5e88de516999182a499b9c08765
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-05 09:58:29 -0600

    Display top word on page load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import $ from 'jquery'
+import $ from 'jquery';
+let domain = 'http://localhost:3000';
+
+const getTopWord = () => {
+  // $('#top-word').html('');
+
+  fetch(`${domain}/api/v1/top_word`)
+    .then(response => response.json())
+    .then(topWordApiResult => displayTopWordResult(topWordApiResult))
+    .catch(error => console.error({ error }));
+};
+
+const displayTopWordResult = (topWordApiResult) => {
+  console.log("Object.keys(topWordApiResult.word) = " + Object.keys(topWordApiResult.word));
+  $('#top-word').append(`
+  <h5 id="top-word-result">
+    ${Object.keys(topWordApiResult.word)}
+  </h5>
+`);
+};
 
 $(document).ready(() => {
-  // have fun!
+  getTopWord();
 })
