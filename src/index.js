b7d99e9d090b02565e35f3b88f746fd9404248e9
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-05 10:44:35 -0600

    Add addNewText function that triggers on click of Break Down button

--- src/index.js
+++ src/index.js
@@ -16,6 +16,26 @@ const displayTopWordResult = (topWordApiResult) => {
 `);
 };
 
+const addNewText = (textInput) => {
+  console.log("textInput = " + textInput);
+  fetch(`${domain}/api/v1/words`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ word: { value: textInput } })
+  })
+    .then(done => {
+      console.log("post request made");
+      getTopWord();
+    })
+    .catch(error => console.log(error));
+};
+
 $(document).ready(() => {
   getTopWord();
+
+  $('#break-down-button').on('click', () => {
+    event.preventDefault(); //don't do a post request automatically
+    console.log("text input = " + $("textarea#text-input").value)
+    // addNewText($("textarea#text-input").value);
+  });
 })
